809cc06a882f4647ee9fc8128aa483dd3afd6443
Matt Staats <staats@google.com>
2017-05-15 16:12:42 +0200

    Readds VM annotation to all tests.

--- test/async_command_event_test.dart
+++ test/async_command_event_test.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@TestOn("vm")
 library webdriver.command_event_test;
 
 import 'package:stack_trace/stack_trace.dart';
--- test/async_mouse_test.dart
+++ test/async_mouse_test.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@TestOn("vm")
 library webdriver.mouse_test;
 
 import 'package:test/test.dart';
