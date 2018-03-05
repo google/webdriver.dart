16a028aed49b08f4f74bd22e7505faa9ff8ae3d7
Max Kim <maxkim@google.com>
2018-03-05 12:57:40 -0800

    Change empty to non-const, non-static.

--- lib/src/async/capabilities.dart
+++ lib/src/async/capabilities.dart
@@ -51,7 +51,7 @@ class Capabilities {
     ..[version] = ''
     ..[platform] = BrowserPlatform.android;
 
-  static Map<String, dynamic> get empty => const <String, dynamic>{};
+  Map<String, dynamic> get empty => new <String, dynamic>{};
 }
 
 /// Browser name constants.
