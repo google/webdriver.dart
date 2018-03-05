404e3d22b832d2041e6b56911ebc7279bc1ad353
Max Kim <maxkim@google.com>
2018-03-05 13:00:16 -0800

    Fix

--- lib/src/async/capabilities.dart
+++ lib/src/async/capabilities.dart
@@ -51,7 +51,7 @@ class Capabilities {
     ..[version] = ''
     ..[platform] = BrowserPlatform.android;
 
-  Map<String, dynamic> get empty => new <String, dynamic>{};
+  static Map<String, dynamic> get empty => new <String, dynamic>{};
 }
 
 /// Browser name constants.
