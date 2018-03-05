79f0d83dcfbc897f6d703f7421881a684b455db8
Max Kim <maxkim@google.com>
2018-03-05 13:20:22 -0800

    Standardize

--- lib/src/async/capabilities.dart
+++ lib/src/async/capabilities.dart
@@ -51,7 +51,7 @@ class Capabilities {
     ..[version] = ''
     ..[platform] = BrowserPlatform.android;
 
-  static Map<String, dynamic> get empty => <String, dynamic>{};
+  static Map<String, dynamic> get empty => new <String, dynamic>();
 }
 
 /// Browser name constants.
