06517de28cf163d0276b8148672b3e295ee8ca04
Kevin Moore <kevmoo@users.noreply.github.com>
2026-03-15 14:39:51 -0700

    Remove unnecessary_ignore and fixed the AsyncXHR client (#340)
    * Fix workflow permissions for publish.yaml

--- lib/src/sync/web_driver.dart
+++ lib/src/sync/web_driver.dart
@@ -23,10 +23,7 @@ import '../common/spec.dart';
 import '../common/utils.dart';
 import '../common/webdriver_handler.dart';
 import 'common.dart';
-// ignore: uri_does_not_exist
-import 'common_stub.dart'
-// ignore: uri_does_not_exist
-    if (dart.library.io) 'common_io.dart';
+import 'common_stub.dart' if (dart.library.io) 'common_io.dart';
 import 'cookies.dart';
 import 'keyboard.dart';
 import 'logs.dart';
